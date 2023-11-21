72d875c6c6e7d87342f3f4cfb91b90ac2905f0eb
BalintBabos <babos819@gmail.com>
2023-11-21 18:49:49 +0100

    Added non-crud methods for Computer in IComputerLogic and ComputerLogic

---
+++ CR3JW9_HFT_2023241.Logic/ComputerLogic.cs
@@ -0,0 +1,63 @@
+﻿using CR3JW9_HFT_2023241.Models;
+using CR3JW9_HFT_2023241.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CR3JW9_HFT_2023241.Logic
+{
+    public class ComputerLogic : IComputerLogic
+    {
+        IRepository<Computer> repo;
+
+        public ComputerLogic(IRepository<Computer> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(Computer item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Computer Read(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<Computer> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Computer item)
+        {
+            this.repo.Update(item);
+        }
+
+        // non-cruds
+        public int? GetNumberOfFastComputers() // has GPU & RAM >16
+        {
+            var result = this.repo.ReadAll().Where(c => c.RAMAmount >= 16 && !string.IsNullOrEmpty(c.GPUModel)).Count();
+            return result;
+        }
+
+        public string? GetOwnerOfComputerByComputerID(int computerID)
+        {
+            var result = this.repo.ReadAll()
+        .Where(c => c.ComputerID == computerID)
+        .Select(c => c.Person.Name)
+        .FirstOrDefault();
+            return result;
+        }
+    }
+}
---
+++ CR3JW9_HFT_2023241.Logic/IComputerLogic.cs
@@ -0,0 +1,21 @@
+﻿using CR3JW9_HFT_2023241.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CR3JW9_HFT_2023241.Logic
+{
+    internal interface IComputerLogic
+    {
+        void Create(Computer item);
+        void Delete(int id);
+        Computer Read(int id);
+        IQueryable<Computer> ReadAll();
+        void Update(Computer item);
+        public int? GetNumberOfFastComputers();
+        public string? GetOwnerOfComputerByComputerID(int computerID);
+    }
+}
