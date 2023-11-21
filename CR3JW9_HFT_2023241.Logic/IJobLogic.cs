407c28ec7d91c149dde8ec4bb266462291cb2621
BalintBabos <babos819@gmail.com>
2023-11-21 18:49:08 +0100

    Added crud and non-crud methods for Job in IJobLogic and JobLogic

---
+++ CR3JW9_HFT_2023241.Logic/IJobLogic.cs
@@ -0,0 +1,24 @@
+﻿using CR3JW9_HFT_2023241.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CR3JW9_HFT_2023241.Logic
+{
+    internal interface IJobLogic
+    {
+        void Create(Job item);
+        void Delete(int id);
+        Job Read(int id);
+        IQueryable<Job> ReadAll();
+        void Update(Job item);
+        string? GetOldestPersonPerJob(int jobID);
+        string? GetYoungestPersonPerJob(int jobID);
+
+        double? GetAverageAgePerJob(int jobID);
+        int? GetCountOfPersonsByJobID(int jobID);
+    }
+}
---
+++ CR3JW9_HFT_2023241.Logic/JobLogic.cs
@@ -0,0 +1,75 @@
+﻿using CR3JW9_HFT_2023241.Models;
+using CR3JW9_HFT_2023241.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CR3JW9_HFT_2023241.Logic
+{
+    public class JobLogic
+    {
+        IRepository<Job> repo;
+
+        public JobLogic(IRepository<Job> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(Job item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Job Read(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<Job> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Job item)
+        {
+            this.repo.Update(item);
+        }
+
+        // non-cruds
+        public string? GetOldestPersonPerJob(int jobID)
+        {
+            var result1 =
+               ReadAll().FirstOrDefault(t => t.JobID == jobID);
+            var result2 = result1.Persons.OrderByDescending(t => t.Age).First();
+            return result2.Name;
+        }
+        public string? GetYoungestPersonPerJob(int jobID)
+        {
+            var result1 =
+               ReadAll().FirstOrDefault(t => t.JobID == jobID);
+            var result2 = result1.Persons.OrderBy(t => t.Age).First();
+            return result2.Name;
+        }
+
+        public double? GetAverageAgePerJob(int jobID)
+        {
+            var result = ReadAll().FirstOrDefault(t => t.JobID == jobID);
+            return result.Persons.Average(t => t.Age);
+        }
+
+        public int? GetNumberOfPeople(int jobID)
+        {
+            var result = this.repo.ReadAll().Include(t => t.Persons).Where(t => t.JobID == jobID).Select(t => t.Persons.Count()).FirstOrDefault();
+            return result;
+        }
+    }
+}
