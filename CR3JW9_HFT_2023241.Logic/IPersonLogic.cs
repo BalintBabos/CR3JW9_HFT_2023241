dea03781dd839306bb62f225a4b163a26e860232
BalintBabos <babos819@gmail.com>
2023-11-21 18:49:26 +0100

    Added non-crud methods for Person in IPersonLogic and PersonLogic

---
+++ CR3JW9_HFT_2023241.Logic/IPersonLogic.cs
@@ -0,0 +1,21 @@
+﻿using CR3JW9_HFT_2023241.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CR3JW9_HFT_2023241.Logic
+{
+    internal interface IPersonLogic
+    {
+        void Create(Person item);
+        void Delete(int id);
+        Person Read(int id);
+        IQueryable<Person> ReadAll();
+        void Update(Person item);
+
+        IEnumerable<Computer> GetCPUModelByJobID(int jobID);
+    }
+}
---
+++ CR3JW9_HFT_2023241.Logic/PersonLogic.cs
@@ -0,0 +1,57 @@
+﻿using CR3JW9_HFT_2023241.Models;
+using CR3JW9_HFT_2023241.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CR3JW9_HFT_2023241.Logic
+{
+    public class PersonLogic
+    {
+        IRepository<Person> repo;
+
+        public PersonLogic(IRepository<Person> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(Person item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Person Read(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<Person> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Person item)
+        {
+            this.repo.Update(item);
+        }
+        // non-cruds
+
+        public IEnumerable<Computer> GetCPUModelByJobID(int jobID)
+        {      
+        var result = this.repo.ReadAll()
+        .Where(t => t.JobID == jobID)
+        .SelectMany(t => t.Computers);
+        return result;
+        }
+    }
+}
