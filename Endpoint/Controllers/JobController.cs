a34c69f331ceacd382af2b1d1501cfdf29b2a4fd
BalintBabos <babos819@gmail.com>
2023-11-22 21:47:16 +0100

    Controllers created in the Endpoint layer

---
+++ Endpoint/Controllers/JobController.cs
@@ -0,0 +1,48 @@
+﻿using CR3JW9_HFT_2023241.Logic;
+using CR3JW9_HFT_2023241.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CR3JW9_HFT_2023241.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class JobController : ControllerBase
+    {
+        IJobLogic logic;
+        public JobController(IJobLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<Job> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public Job Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] Job value)
+        {
+            this.logic.Create(value);
+        }
+
+        [HttpPut]
+        public void Put([FromBody] Job value)
+        {
+            this.logic.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
+    }
+}
---
+++ Endpoint/Controllers/PersonController.cs
@@ -0,0 +1,48 @@
+﻿using CR3JW9_HFT_2023241.Logic;
+using CR3JW9_HFT_2023241.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CR3JW9_HFT_2023241.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        IPersonLogic logic;
+        public PersonController(IPersonLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<Person> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public Person Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] Person value)
+        {
+            this.logic.Create(value);
+        }
+
+        [HttpPut]
+        public void Put([FromBody] Person value)
+        {
+            this.logic.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
+    }
+}
