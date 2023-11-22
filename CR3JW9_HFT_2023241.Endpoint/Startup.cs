e62b0e39b36afaffff4cd46f7cc0c1cac5bb7bc0
BalintBabos <babos819@gmail.com>
2023-11-22 21:41:08 +0100

    Errors corrected in the Endpoint

--- CR3JW9_HFT_2023241.Endpoint/Startup.cs
+++ CR3JW9_HFT_2023241.Endpoint/Startup.cs
@@ -1,21 +1,51 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using CR3JW9_HFT_2023241.Logic;
+using CR3JW9_HFT_2023241.Models;
+using CR3JW9_HFT_2023241.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using CR3JW9_HFT_2023241.Repository.ModelRepositories;
 
-namespace CR3JW9_HFT_2023241.Endpoint
+namespace MovieDbApp.Endpoint
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<WorkDbContext>();
+
+            services.AddTransient<IRepository<Person>, PersonRepository>();
+            services.AddTransient<IRepository<Job>, JobRepository>();
+            services.AddTransient<IRepository<Computer>, ComputerRepository>();
+
+            services.AddTransient<IPersonLogic, PersonLogic>();
+            services.AddTransient<IJobLogic, JobLogic>();
+            services.AddTransient<IComputerLogic, ComputerLogic>();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CR3JW9_HFT_2023241.Endpoint", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,16 +54,27 @@ namespace CR3JW9_HFT_2023241.Endpoint
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CR3JW9_HFT_2023241.Endpoint v1"));
             }
+
+            app.UseExceptionHandler(c => c.Run(async context =>
+            {
+                var exception = context.Features
+                    .Get<IExceptionHandlerPathFeature>()
+                    .Error;
+                var response = new { Msg = exception.Message };
+                await context.Response.WriteAsJsonAsync(response);
+            }));
+
 
             app.UseRouting();
 
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
