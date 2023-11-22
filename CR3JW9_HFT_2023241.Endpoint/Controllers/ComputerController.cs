f4b4e8780933d92a469c21a3e820622e8a8f54a3
BalintBabos <babos819@gmail.com>
2023-11-22 21:01:59 +0100

    Controllers finished

---
+++ CR3JW9_HFT_2023241.Endpoint/Controllers/ComputerController.cs
@@ -0,0 +1,48 @@
+﻿using CR3JW9_HFT_2023241.Logic;
+using CR3JW9_HFT_2023241.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CR3JW9_HFT_2023241.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ComputerController : ControllerBase
+    {
+        IComputerLogic logic;
+        public ComputerController(IComputerLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<Computer> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public Computer Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] Computer value)
+        {
+            this.logic.Create(value);
+        }
+
+        [HttpPut]
+        public void Put([FromBody] Computer value)
+        {
+            this.logic.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
+    }
+}
---
+++ CR3JW9_HFT_2023241.Endpoint/Controllers/StatController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CR3JW9_HFT_2023241.Logic;
+using System.Collections.Generic;
+
+namespace CR3JW9_HFT_2023241.Endpoint.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class StatController : ControllerBase
+    {
+        //IPersonLogic logic;
+
+        //public StatController(IPersonLogic logic)
+        //{
+        //    this.logic = logic;
+        //}
+
+        //[HttpGet("{CPU}")]
+        //public IEnumerable<Computer> GetCPUModelByJobID(int jobID)
+        //{
+        //    return this.logic.GetCPUModelByJobID(jobID);
+        //}
+
+       
+    }
+}
