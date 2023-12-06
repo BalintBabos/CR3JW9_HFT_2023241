64e30325f6973bc7f0ae22bf396644eb930a67a1
BalintBabos <babos819@gmail.com>
2023-12-06 17:33:54 +0100

    Included all Non-cruds methods in the StatController class

--- Endpoint/Controllers/StatController.cs
+++ Endpoint/Controllers/StatController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using CR3JW9_HFT_2023241.Logic;
 using System.Collections.Generic;
+using CR3JW9_HFT_2023241.Models;
 
 namespace CR3JW9_HFT_2023241.Endpoint.Controllers
 {
@@ -8,19 +9,57 @@ namespace CR3JW9_HFT_2023241.Endpoint.Controllers
     [ApiController]
     public class StatController : ControllerBase
     {
-        //IPersonLogic logic;
+        IPersonLogic personLogic;
+        IJobLogic jobLogic;
+        IComputerLogic computerLogic;
 
-        //public StatController(IPersonLogic logic)
-        //{
-        //    this.logic = logic;
-        //}
+        public StatController(IPersonLogic personLogic, IJobLogic jobLogic, IComputerLogic computerLogic)
+        {
+            this.personLogic = personLogic;
+            this.jobLogic = jobLogic;        
+            this.computerLogic = computerLogic;
+        }
 
-        //[HttpGet("{CPU}")]
-        //public IEnumerable<Computer> GetCPUModelByJobID(int jobID)
-        //{
-        //    return this.logic.GetCPUModelByJobID(jobID);
-        //}
+        [HttpGet("{jobID}")]
+        public IEnumerable<Computer> GetCPUModelByJobID(int jobID)
+        {
+            return this.personLogic.GetCPUModelByJobID(jobID);
+        }
 
-       
+        [HttpGet("{jobID}")]
+        public string? GetOldestPersonPerJob(int jobID)
+        {
+            return this.jobLogic.GetOldestPersonPerJob(jobID);
+        }
+        [HttpGet("{jobID}")]
+        public string? GetYoungestPersonPerJob(int jobID)
+        {
+            return this.jobLogic.GetYoungestPersonPerJob(jobID);
+        }
+
+        [HttpGet("{jobID}")]
+        public double? GetAverageAgePerJob(int jobID)
+        {
+            return this.jobLogic.GetAverageAgePerJob(jobID);
+        }
+
+        [HttpGet("{jobID}")]
+        public int? GetNumberOfPeople(int jobID)
+        {
+            return this.jobLogic.GetNumberOfPeople(jobID);
+        }
+
+        [HttpGet]
+        public int? GetNumberOfFastComputers()
+        {
+            return this.computerLogic.GetNumberOfFastComputers();
+        }
+
+        [HttpGet("{computerID}")]
+        public string? GetOwnerOfComputerByComputerID(int computerID)
+        {
+            return this.computerLogic.GetOwnerOfComputerByComputerID(computerID);
+        }
+
     }
 }
