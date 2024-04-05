3d67c70f04cd0c9394fe48057225c34cdd304be7
BalintBabos <babos819@gmail.com>
2024-04-05 15:24:56 +0200

    ComputerWindow created

---
+++ CR3JW9_HFT_2023241.WpfClient/WindowModels/ComputerWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+using CR3JW9_HFT_2023241.Models;
+
+namespace CR3JW9_HFT_2023241.WpfClient.WindowModels
+{
+    /// <summary>
+    /// Interaction logic for ComputerWindow.xaml
+    /// </summary>
+    public partial class ComputerWindow : Window
+    {
+        public ComputerWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
