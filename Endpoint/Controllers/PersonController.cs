0bc89d2d42f8bade07b5033d610402eff0890a5b
BalintBabos <babos819@gmail.com>
2024-04-05 15:32:52 +0200

    PersonController updated with SignalR

--- Endpoint/Controllers/PersonController.cs
+++ Endpoint/Controllers/PersonController.cs
@@ -1,6 +1,8 @@
-﻿using CR3JW9_HFT_2023241.Logic;
+﻿using CR3JW9_HFT_2023241.Endpoint.Services;
+using CR3JW9_HFT_2023241.Logic;
 using CR3JW9_HFT_2023241.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using System.Collections.Generic;
 
 namespace CR3JW9_HFT_2023241.Endpoint.Controllers
@@ -10,9 +12,11 @@ namespace CR3JW9_HFT_2023241.Endpoint.Controllers
     public class PersonController : ControllerBase
     {
         IPersonLogic logic;
-        public PersonController(IPersonLogic logic)
+        IHubContext<SignalRHub> hub;
+        public PersonController(IPersonLogic logic, IHubContext<SignalRHub> hub)
         {
             this.logic = logic;
+            this.hub = hub;
         }
 
         [HttpGet]
@@ -31,18 +35,22 @@ namespace CR3JW9_HFT_2023241.Endpoint.Controllers
         public void Create([FromBody] Person value)
         {
             this.logic.Create(value);
+            this.hub.Clients.All.SendAsync("PersonCreated", value);
         }
 
         [HttpPut]
         public void Update([FromBody] Person value)
         {
             this.logic.Update(value);
+            this.hub.Clients.All.SendAsync("PersonUpdated", value);
         }
 
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            var personToDelete = this.logic.Read(id);
             this.logic.Delete(id);
+            this.hub.Clients.All.SendAsync("PersonDeleted", personToDelete);
         }
     }
 }
