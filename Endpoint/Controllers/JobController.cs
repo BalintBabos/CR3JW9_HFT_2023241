71ce6e1823c5ccb7176d5d411c566313ccc22f35
BalintBabos <babos819@gmail.com>
2024-04-05 15:31:14 +0200

    JobController updated with SignalR

--- Endpoint/Controllers/JobController.cs
+++ Endpoint/Controllers/JobController.cs
@@ -1,6 +1,8 @@
-﻿using CR3JW9_HFT_2023241.Logic;
+﻿using CR3JW9_HFT_2023241.Endpoint.Services;
+using CR3JW9_HFT_2023241.Logic;
 using CR3JW9_HFT_2023241.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using System.Collections.Generic;
 
 namespace CR3JW9_HFT_2023241.Endpoint.Controllers
@@ -10,9 +12,11 @@ namespace CR3JW9_HFT_2023241.Endpoint.Controllers
     public class JobController : ControllerBase
     {
         IJobLogic logic;
-        public JobController(IJobLogic logic)
+        IHubContext<SignalRHub> hub;
+        public JobController(IJobLogic logic, IHubContext<SignalRHub> hub)
         {
             this.logic = logic;
+            this.hub = hub;
         }
 
         [HttpGet]
@@ -31,18 +35,22 @@ namespace CR3JW9_HFT_2023241.Endpoint.Controllers
         public void Create([FromBody] Job value)
         {
             this.logic.Create(value);
+            this.hub.Clients.All.SendAsync("JobCreated", value);
         }
 
         [HttpPut]
         public void Update([FromBody] Job value)
         {
             this.logic.Update(value);
+            this.hub.Clients.All.SendAsync("JobUpdated", value);
         }
 
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            var jobToDelete = this.logic.Read(id);
             this.logic.Delete(id);
+            this.hub.Clients.All.SendAsync("JobDeleted", jobToDelete);
         }
     }
 }
