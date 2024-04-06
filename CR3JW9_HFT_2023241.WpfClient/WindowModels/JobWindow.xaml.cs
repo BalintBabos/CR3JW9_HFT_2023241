54decf8776d6b96976e1163911a15da311179d02
BalintBabos <babos819@gmail.com>
2024-04-06 11:34:27 +0200

    JobWindow created

---
+++ CR3JW9_HFT_2023241.WpfClient/WindowModels/JobWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CR3JW9_HFT_2023241.WpfClient.WindowModels
+{
+    /// <summary>
+    /// Interaction logic for JobWindow.xaml
+    /// </summary>
+    public partial class JobWindow : Window
+    {
+        public JobWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
