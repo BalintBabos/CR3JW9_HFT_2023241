c9a3101368f184b492479b6734b8abe18d1d7e42
BalintBabos <babos819@gmail.com>
2024-04-07 13:07:50 +0200

    PersonViaWindow updated Services done

--- CR3JW9_HFT_2023241.WpfClient/Services/PersonViaWindow.cs
+++ CR3JW9_HFT_2023241.WpfClient/Services/PersonViaWindow.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CR3JW9_HFT_2023241.Models;
+using CR3JW9_HFT_2023241.WpfClient.WindowModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,11 @@ using System.Threading.Tasks;
 
 namespace CR3JW9_HFT_2023241.WpfClient.Services
 {
-    internal class PersonViaWindow
+    internal class PersonViaWindow : IPersonService
     {
+        public void Open(RestCollection<Person> people)
+        {
+            new PersonWindow(people).Show();
+        }
     }
 }
