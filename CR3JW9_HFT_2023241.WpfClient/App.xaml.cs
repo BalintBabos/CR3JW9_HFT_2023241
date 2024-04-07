456b7438165b457b97203b09fc2edbf3cd1207d3
BalintBabos <babos819@gmail.com>
2024-04-07 12:27:06 +0200

    IoCContainer implemented in App.xaml.cs

--- CR3JW9_HFT_2023241.WpfClient/App.xaml.cs
+++ CR3JW9_HFT_2023241.WpfClient/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.DependencyInjection;
+using CR3JW9_HFT_2023241.WpfClient.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +16,13 @@ namespace CR3JW9_HFT_2023241.WpfClient
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            Ioc.Default.ConfigureServices(
+               new ServiceCollection()
+                   .AddSingleton<IComputerService, ComputerViaWindow>()
+                   .BuildServiceProvider()
+                   );
+        }      
     }
 }
