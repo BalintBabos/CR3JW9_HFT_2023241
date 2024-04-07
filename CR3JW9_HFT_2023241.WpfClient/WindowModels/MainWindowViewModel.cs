e1f0a0601d03517fc9314d6592516fda9c476b2a
BalintBabos <babos819@gmail.com>
2024-04-07 13:08:56 +0200

    MainWindowViewModel updated ViewModels done

--- CR3JW9_HFT_2023241.WpfClient/WindowModels/MainWindowViewModel.cs
+++ CR3JW9_HFT_2023241.WpfClient/WindowModels/MainWindowViewModel.cs
@@ -17,10 +17,16 @@ namespace CR3JW9_HFT_2023241.WpfClient.WindowModels
     class MainWindowViewModel : ObservableRecipient
     {
         IComputerService computerService;
+        IJobService jobService;
+        IPersonService personService;
 
         public RestCollection<Computer> computers { get; set; }
+        public RestCollection<Job> jobs { get; set; }
+        public RestCollection<Person> people { get; set; }
 
         public ICommand GetComputersCommand { get; set; }
+        public ICommand GetJobsCommand { get; set; }
+        public ICommand GetPeopleCommand { get; set; }
 
         public static bool IsInDesignMode
         {
@@ -35,16 +41,27 @@ namespace CR3JW9_HFT_2023241.WpfClient.WindowModels
             if (!IsInDesignMode)
             {
                 computers = new RestCollection<Computer>("http://localhost:59537/", "Computer", "hub");
+                jobs = new RestCollection<Job>("http://localhost:59537/", "Job", "hub");
+                people = new RestCollection<Person>("http://localhost:59537/", "Person", "hub");
 
 
                 computerService = Ioc.Default.GetRequiredService<IComputerService>();
+                jobService = Ioc.Default.GetRequiredService<IJobService>();
+                personService = Ioc.Default.GetRequiredService<IPersonService>();
 
                 GetComputersCommand = new RelayCommand(
                     () => computerService.Open(computers),
                     () => true
                     );
+                GetJobsCommand = new RelayCommand(
+                    () => jobService.Open(jobs),
+                    () => true
+                    );
+                GetPeopleCommand = new RelayCommand(
+                    () => personService.Open(people),
+                    () => true
+                    );
             }
         }
-
     }
 }
