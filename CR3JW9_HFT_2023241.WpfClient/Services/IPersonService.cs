bee9f6c077573335bf184c661ac0bbedf81cfdfe
BalintBabos <babos819@gmail.com>
2024-04-07 13:07:23 +0200

    IPersonService updated

--- CR3JW9_HFT_2023241.WpfClient/Services/IPersonService.cs
+++ CR3JW9_HFT_2023241.WpfClient/Services/IPersonService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CR3JW9_HFT_2023241.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace CR3JW9_HFT_2023241.WpfClient.Services
 {
-    internal class IPersonService
+    interface IPersonService
     {
+        public void Open(RestCollection<Person> people);
     }
 }
